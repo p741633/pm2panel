c1dee3c9e122f1db1140855af06abf11839c59e5
PiriyaV <piriya_v@gpo.or.th>
2020-11-04 15:58:27 +0700

    Eslint

--- pm2panel.js
+++ pm2panel.js
@@ -1,13 +1,14 @@
-//##############################################################################
+/* eslint-disable handle-callback-err */
+// ##############################################################################
 //                             inital packages
-//##############################################################################
+// ##############################################################################
 
 const path = require('path');
 const express = require('express');
 const app = express();
 const exec = require('child_process').exec;
 const fs = require('fs');
-const { pamAuthenticate, pamErrors } = require('node-linux-pam');
+const { pamAuthenticate } = require('node-linux-pam');
 const dotenv = require('dotenv');
 
 var session = require('express-session');
@@ -15,9 +16,9 @@ var session = require('express-session');
 // .env
 dotenv.config();
 
-//##############################################################################
+// ##############################################################################
 //                             config panel
-//##############################################################################
+// ##############################################################################
 const PORT = process.env.PORT;
 const PAM_AUTH = false; // if set to true, USER and PASS won't be used
 const USER = process.env.USERNAME;
@@ -34,9 +35,9 @@ app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 //
-//##############################################################################
+// ##############################################################################
 //                             rounting urls
-//##############################################################################
+// ##############################################################################
 
 app.get('/', function (req, res) {
     // check is login
@@ -75,9 +76,9 @@ app.post('/loginCheck', function (req, res) {
                     });
                 } else {
                     // user or password incorrect go back to login and logging PAM code if not 7 (invalid credentials)
-                    if (code != 7) console.log('Unsuccessful PAM authentication, code: ' + code);
+                    if (code !== 7) console.log('Unsuccessful PAM authentication, code: ' + code);
                     res.writeHead(302, {
-                        Location: '/login?err=' + (code == 7 ? 'invalid_credentials' : 'system'),
+                        Location: '/login?err=' + (code === 7 ? 'invalid_credentials' : 'system'),
                     });
                 }
 
@@ -86,7 +87,7 @@ app.post('/loginCheck', function (req, res) {
         );
     } else {
         // check username and password by local authentication
-        if (req.body.username === USER && req.body.passwd == PASS) {
+        if (req.body.username === USER && req.body.passwd === PASS) {
             // login process
             req.session.islogin = true;
             // redirect to panel
@@ -117,7 +118,7 @@ app.get('/getProccess', function (req, res) {
         });
         // get json list from the json
         exec('pm2 jlist', (error, stdout, stderr) => {
-            //do whatever here
+            // do whatever here
             res.write(stdout);
             res.end();
         });
@@ -289,7 +290,7 @@ app.get('/dump', function (req, res) {
             res.writeHead(302, {
                 Location: '/',
             });
-            //req.session.notication = error + '\n--------\n' + stdout + '\n--------\n' + stderr;
+            // req.session.notication = error + '\n--------\n' + stdout + '\n--------\n' + stderr;
             if (error != null) {
                 req.session.notication = error + stderr;
             } else {
@@ -329,11 +330,12 @@ app.get('/folder', function (req, res) {
         });
         res.end();
     } else {
+        var chossedPath;
         // check path and set default tab
         if (req.query.path === undefined) {
-            var chossedPath = '/';
+            chossedPath = '/';
         } else {
-            var chossedPath = req.query.path;
+            chossedPath = req.query.path;
         }
         // send json header
         res.writeHead(200, {
@@ -405,9 +407,9 @@ app.get('/log', function (req, res) {
     }
 });
 
-//##############################################################################
+// ##############################################################################
 //                              finazle
-//##############################################################################
+// ##############################################################################
 
 app.listen(PORT, function () {
     console.log('pm2panel app listening on port ' + PORT + '! \n test: http://localhost:' + PORT);
